8446a65acec56e9993d2763a6595058ca0e380bf
soflil <134602526+soflil@users.noreply.github.com>
2023-11-02 11:53:52 +0100

    Added the functionality of the select menu and fixed a bug from the earlier commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ function timeDateData() {
     .format("h:mm:ss [<small>]A[</small>]");
 
   let osloElement = document.querySelector("#oslo");
-  let osloDateElement = osloElement.querySelector(".date");
-  let osloTimeElement = osloElement.querySelector(".time");
-  osloDateElement.innerHTML = osloDate;
-  osloTimeElement.innerHTML = osloTime;
+  if (osloElement) {
+    let osloDateElement = osloElement.querySelector(".date");
+    let osloTimeElement = osloElement.querySelector(".time");
+    osloDateElement.innerHTML = osloDate;
+    osloTimeElement.innerHTML = osloTime;
+  }
 
   let istanbulDate = moment().tz("Europe/Istanbul").format("Do MMMM YYYY");
   let istanbulTime = moment()
@@ -16,11 +18,33 @@ function timeDateData() {
     .format("h:mm:ss [<small>]A[</small>]");
 
   let istanbulElement = document.querySelector("#istanbul");
-  let istanbulDateElement = istanbulElement.querySelector(".date");
-  let istanbulTimeElement = istanbulElement.querySelector(".time");
-  istanbulDateElement.innerHTML = istanbulDate;
-  istanbulTimeElement.innerHTML = istanbulTime;
+  if (istanbulElement) {
+    let istanbulDateElement = istanbulElement.querySelector(".date");
+    let istanbulTimeElement = istanbulElement.querySelector(".time");
+    istanbulDateElement.innerHTML = istanbulDate;
+    istanbulTimeElement.innerHTML = istanbulTime;
+  }
 }
 
 timeDateData();
 setInterval(timeDateData, 1000);
+
+function changeCity(event) {
+  let citySelected = event.target.value;
+  let cityTimezone = moment().tz(citySelected);
+  let cityName = citySelected.replace("_", " ").split("/")[1];
+
+  let citiesElement = document.querySelector(".cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTimezone.format("Do MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTimezone.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
+let citySelect = document.querySelector("#select-city");
+citySelect.addEventListener("change", changeCity);
